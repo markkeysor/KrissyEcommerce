26374684c25fb5ce463d53a367b814891cb7d948
Mark Keysor <markkeysor@gmail.com>
2015-09-22 15:01:19 -0600

    added app.js

---
+++ public/app.js
@@ -0,0 +1,32 @@
+var app = angular.module('krissy', [ui.router]);
+
+app.config(function($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'app/home/templates/homeTmpl.html',
+      controller: 'homeCtrl'
+    })
+
+    .state('products', {
+      url: '/products',
+      templateUrl: 'app/products/templates/productsTmpl.html',
+      controller: 'productsCtrl'
+    })
+
+    .state('about', {
+      url: '/about',
+      templateUrl: 'app/about/templates/aboutTmpl.html',
+      controller: 'aboutCtrl'
+    })
+
+    .state('contact', {
+      url: '/contact',
+      templateUrl: 'app/contact/templates/contactTmpl.html',
+      controller: 'contactCtrl'
+    })
+
+});
